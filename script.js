94ca6c03163829d61c278a757be24a387d049068
Avery Yang <averyyang2674@gmail.com>
2022-05-10 16:39:42 -0500

    add sounds to each pokemon

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-function play() {
-    var audio = document.getElementById("bulbasaur");
+///////////////////////////////////Sounds for each pokemon//////////////////////////////////////
+document.querySelector('#bulbasaur').addEventListener('click', function () {
+    var audio = new Audio('sounds/bulbasaur.mp3');
     audio.play();
-  }
+});
 
-  
+document.querySelector('#charmander').addEventListener('click', function () {
+    var audio = new Audio('sounds/charmander.mp3');
+    audio.play();
+});
+
+document.querySelector('#squirtle').addEventListener('click', function () {
+    var audio = new Audio('sounds/squirtle.mp3');
+    audio.play();
+});
